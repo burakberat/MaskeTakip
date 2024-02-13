9099b168ce98f37760efd970878f075bbdd77d7f
Burak Berat Orucoglu <burakorucoglu@outlook.com>
2024-02-13 23:44:11 +0300

    Proje Eklendi.

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -1,9 +1,19 @@
 ﻿
 
+using Business.Concrete;
+using Entities.Concrete;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Person person1 = new Person();
+        person1.FirstName = "Ahmet";
+        person1.LastName = "Mehmet";
+        person1.DateOfBirthYear = 1985;
+        person1.NationalIdentity = 123456;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
     }
 }
